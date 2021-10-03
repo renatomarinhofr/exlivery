46dd8175bf79f3d9926652158bef0fdd6a5ae89f
Renato Marinho <renato.marinhofr@gmail.com>
2021-10-02 23:51:16 -0300

    facilitando a criação do user

--- lib/exlivery.ex
+++ lib/exlivery.ex
@@ -1,2 +1,10 @@
 defmodule Exlivery do
+  alias Exlivery.Users.Agent, as: UserAgent
+  alias Exlivery.Users.CreateOrUpdate
+
+  def start_agents do
+    UserAgent.start_link(%{})
+  end
+
+  defdelegate create_or_update_user(params), to: CreateOrUpdate, as: :call
 end
