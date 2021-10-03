cb3af8c294bdcb32926fce5f9c4e9c90af55c0b1
Renato Marinho <renato.marinhofr@gmail.com>
2021-10-02 22:57:03 -0300

    Testando a struct de Order

--- test/support/factory.ex
+++ test/support/factory.ex
@@ -1,8 +1,8 @@
 defmodule Exlivery.Factory do
   use ExMachina
 
+  alias Exlivery.Orders.{Item, Order}
   alias Exlivery.Users.User
-  alias Exlivery.Orders.Item
 
   def user_factory do
     %User{
@@ -20,6 +20,23 @@ defmodule Exlivery.Factory do
       category: :pizza,
       quantity: 1,
       unity_price: Decimal.new("35.5")
+    }
+  end
+
+  def order_factory do
+    %Order{
+      delivery_address: "Rua das bananeiras, 55",
+      items: [
+        build(:item),
+        build(:item,
+          description: "Temaki de atum",
+          category: :japonesa,
+          quantity: 2,
+          unity_price: Decimal.new("20.50")
+        )
+      ],
+      total_price: Decimal.new("76.50"),
+      user_cpf: "12345678900"
     }
   end
 end
